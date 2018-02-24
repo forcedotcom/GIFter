083f7f77e56bb57fad5db2998ddfd16e406aa2b5
Wade Wegner <wade.wegner@gmail.com>
2018-02-24 07:09:36 -0800

    GIPHY searches working

--- force-app/main/default/aura/SearchGIPHY/SearchGIPHYController.js
+++ force-app/main/default/aura/SearchGIPHY/SearchGIPHYController.js
@@ -1,5 +1,42 @@
 ({
-    myAction : function(component, event, helper) {
+
+  afterScriptsLoaded: function (component, event, helper) {
+    console.log('afterScriptsLoaded called successfully');
+  },
+
+  doInit: function (component, event, helper) {
+    console.log('doInit called successfully');
+  },
+
+  keyCheck: function (component, event, helper) {
+    if (event.which == 13) {
+      console.log('keyCheck detected enter')
+
+      var searchTerms = component.get("v.searchTerms");
+      helper.search(searchTerms, function (results) {
+        console.log('keyCheck enter returned from helper')
+        component.set("v.results", results.data);
+      });
 
     }
+  },
+
+  search: function (component, event, helper) {
+
+    console.log('search called successfully');
+
+    var searchTerms = component.get("v.searchTerms");
+    helper.search(searchTerms, function (results) {
+      console.log('search returned from helper')
+      component.set("v.results", results.data);
+    });
+
+  },
+
+  gifSelected: function (component) {
+    var property = component.get("v.property");
+
+    console.log('gifSelected called successfully');
+  }
+
 })
